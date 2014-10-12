8181e546f935b02e0552a04eca1cf879a141ced2
Steve <gon138@gmail.com>
2014-10-11 22:35:07 -0700

    pass all tests

--- src/people/tenant.js
+++ src/people/tenant.js
@@ -2,22 +2,27 @@ var Person = require("./person.js");
 
 function Tenant(name, contact) {
   // inherits name contact from Person
-  // ...
+  Person.call(this, name, contact);
   // tennant has 'array' of references
-  // ...
+  this.references = [];
 };
 
 // Set prototype and constructor
-// ...
+  Tenant.prototype = new Person;
+  Tenant.prototype.constructor = Tenant;
 
 Tenant.prototype.addReference = function(reference){
   // add reference to references. Reference must be of type Person
-  // ...
+  if (reference instanceof Person) {
+  	this.references.push(reference);
+  }
 };
 
 Tenant.prototype.removeReference = function(reference) {
   // remove reference from references.
-  // ...
+  if (reference instanceof Person) {
+  	this.references.splice(this.references.indexOf(reference),1);
+  }
 };
 
 module.exports = Tenant;
