3e858bc6e7ee0b975f7710c21b17b264dffb2508
rragno <robertragno@gmail.com>
2014-10-14 08:14:46 -0700

    First part of HW complete

--- src/rental_property/unit.js
+++ src/rental_property/unit.js
@@ -1,12 +1,21 @@
 function Unit (number, building, sqft, rent) {
   // set params above as instance variables
-  // ...
+  this.number = number;
+  this.building = building;
+  this.sqft = sqft;
+  this.rent = rent;
   // Unit has also a tenant
-  // ...
+  this.tenant = null;
 }
 
 Unit.prototype.available = function(){
   // Returns true if unit is available, otherwise false
+  if (this.tenant === null) {
+  	return true;
+  }
+  else {
+  	return false;
+  }
 }
 
 // export the module
